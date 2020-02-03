6cc157239776d1e41a378c102c639b5e068bfd8d
Brayan Esteves <werneriasearch@gmail.com>
2020-02-03 00:47:36 -0400

    Create index.py

---
+++ index.py
@@ -0,0 +1,25 @@
+import string
+import random
+
+def generatePassword(num):
+	password = ''	
+	for n in range(num):
+	    x = random.randint(0,94)
+	    password += string.printable[x]	
+	return password
+
+
+if __name__ == '__main__':
+        
+    op = 0
+    while(op!=2):
+
+        print("1.- GENERATE PASSWORD ")
+
+        print("2.- EXIT")        
+
+        op = int(input("Input Option: "))
+
+        if (op == 1):
+            _password = generatePassword(16)
+            print(_password)
